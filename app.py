c2d454e10d8282b0cc45964c1d738891f4e2bad4
sterlingfancher-cmyk <sterling.fancher@gmail.com>
2026-04-09 18:16:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import joblib
 app = Flask(__name__)
 
 TRADIER_API_KEY = "YOUR_TRADIER_API_KEY"
-POLYGON_API_KEY = "YOUR_POLYGON_API_KEY"
+POLYGON_API_KEY = "u1sj82NBjSYn4lkvHxn5eMZyXAVaEIlk"
 ACCOUNT_ID = "YOUR_ACCOUNT_ID"
 
 BASE_URL = "https://api.tradier.com/v1"
