95477a6a735fa6247e29c05572805dc9b294d39d
sterlingfancher-cmyk <sterling.fancher@gmail.com>
2026-04-09 22:14:30 -0500

    Update app.py

--- app.py
+++ app.py
@@ -38,6 +38,11 @@ def compute_strategy(df):
 
     # Strategy returns
     df["strategy_returns"] = df["returns"] * df["position"].shift(1)
+    df["strategy_returns"] = df[strategy returns"].clip(lower=-0.01, upper=0.02)
+
+    # Volatility filter
+    df["volatility"] = df["returns"].rolling(100.std()
+    (df["volatility"] < df["volatility"].rolling(50).mean())                                         
 
     # FINAL safety check (DO NOT REMOVE EVERYTHING)
     if len(df) < 5:
