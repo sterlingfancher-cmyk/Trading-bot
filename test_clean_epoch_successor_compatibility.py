b2ab618ab522af76f9677a2714d5dcf2543a7bfe
sterlingfancher-cmyk <sterling.fancher@gmail.com>
2026-08-12 16:43:49 -0500

    Test clean epoch successor compatibility

---
+++ test_clean_epoch_successor_compatibility.py
@@ -0,0 +1,39 @@
+import types
+
+import clean_epoch_successor_compatibility as compat
+
+
+def _core(epoch):
+    core = types.SimpleNamespace()
+    core.portfolio = {"paper_accounting_epoch": epoch}
+    return core
+
+
+def test_exact_verified_successor_is_recognized():
+    core = _core({
+        "id": compat.NEW_EPOCH_ID,
+        "prior_epoch_id": compat.OLD_EPOCH_ID,
+        "historical_recovery_decision": "verified_snapshot_rollforward",
+        "historical_evidence_archived": True,
+    })
+    assert compat._is_verified_successor(core) is True
+
+
+def test_unrelated_epoch_is_not_recognized():
+    core = _core({
+        "id": "some-other-epoch",
+        "prior_epoch_id": compat.OLD_EPOCH_ID,
+        "historical_recovery_decision": "verified_snapshot_rollforward",
+        "historical_evidence_archived": True,
+    })
+    assert compat._is_verified_successor(core) is False
+
+
+def test_successor_requires_archived_evidence():
+    core = _core({
+        "id": compat.NEW_EPOCH_ID,
+        "prior_epoch_id": compat.OLD_EPOCH_ID,
+        "historical_recovery_decision": "verified_snapshot_rollforward",
+        "historical_evidence_archived": False,
+    })
+    assert compat._is_verified_successor(core) is False
